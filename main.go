9633e00cdb100e1328ecb551ade29edf09c31ac9
Qther <dtaclanlegion@gmail.com>
2021-12-02 21:33:07 +0800

    Improve days 1 & 2, refactor, add wait mode, get prompt, & post results

--- main.go
+++ main.go
@@ -6,30 +6,52 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/gen2brain/beeep"
 )
 
 var token string
-var years = map[int]func(int){
+var years = map[int]func(int, int){
 	2021: year2021,
 }
 
 func main() {
 	var year int
 	var day int
+	var level int
+	var prompt bool
+	var wait int64
 	now := time.Now().UTC().Add(6 * time.Hour)
 
 	flag.IntVar(&year, "year", now.Year(), "Year")
 	flag.IntVar(&day, "day", now.Day(), "Day")
+	flag.IntVar(&level, "level", 1, "Level")
 	flag.StringVar(&token, "token", "", "Session Token")
+	flag.BoolVar(&prompt, "prompt", false, "Show Prompt")
+	flag.Int64Var(&wait, "wait", -1, "Wait for AoC until seconds before start")
+
 	flag.Parse()
 
-	if token == "" && !inputExists(year, day) {
-		fmt.Println("Please provide a session token")
-		os.Exit(1)
-		flag.Usage()
+	if wait != -1 {
+		aocStart := time.Date(year, now.Month(), day, 0, 0, 0, 0, time.UTC).Add(6 * time.Hour)
+		fmt.Printf("AoC starts at %s\n", aocStart.Local())
+		for {
+			if now.Before(aocStart.Add(-time.Duration(wait) * time.Second)) {
+				time.Sleep(time.Duration(aocStart.UnixNano() - now.UnixNano() - time.Duration(wait).Nanoseconds()))
+			} else {
+				break
+			}
+		}
+		beeep.Alert("Advent of Code", fmt.Sprintf("AoC %d Day %d starts in %d seconds!", year, day, wait), "assets/icon.png")
+		prompt = true
+	}
+
+	if prompt {
+		fmt.Println(getPrompt(year, day))
 		return
 	}
 
@@ -40,7 +62,7 @@ func main() {
 	}
 	fmt.Printf("Running Year %d Day %d\n", year, day)
 
-	years[year](day)
+	years[year](day, level)
 }
 
 func inputExists(year, day int) bool {
@@ -65,6 +87,13 @@ func readInput(year, day int) []string {
 		}
 
 		return strings.Split(string(b), "\n")
+	}
+
+	if token == "" {
+		fmt.Println("Please provide a session token")
+		flag.Usage()
+		os.Exit(1)
+		return nil
 	}
 
 	// Inputs are located in https://adventofcode.com/<year>/day/<day>/input
@@ -111,30 +140,118 @@ func readInput(year, day int) []string {
 	return strings.Split(string(body), "\n")
 }
 
-func year2021(day int) {
+func getPrompt(year, day int) string {
+	// Prompt is located in https://adventofcode.com/<year>/day/<day>
+	fmt.Print("Downloading prompt from adventofcode.com\n\n")
+
+	res, err := http.Get(fmt.Sprintf("https://adventofcode.com/%d/day/%d", year, day))
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	if res.StatusCode != 200 {
+		fmt.Println(string(body))
+		panic("Failed to download prompt")
+	}
+
+	html := string(body)
+	re := regexp.MustCompile("<(.|\n)*?>")
+	html = re.ReplaceAllString(html, "")
+
+	//Prompt starts after the line beginning with "window.addEventListener" and ends before the line beginning with "To play"
+	lines := strings.Split(html, "\n")
+	for i, line := range lines {
+		if strings.Contains(line, "window.addEventListener") {
+			lines = lines[i+1:]
+			break
+		}
+	}
+
+	for i, line := range lines {
+		if strings.Contains(line, "To play") {
+			lines = lines[:i]
+			break
+		}
+	}
+
+	return strings.Join(lines, "\n")
+}
+
+func postAnswers(year, day, level int, answer string) bool {
+	if token == "" {
+		fmt.Println("Please provide a session token")
+		flag.Usage()
+		os.Exit(1)
+		return false
+	}
+
+	// Answers are posted to https://adventofcode.com/<year>/day/<day>/answer
+	fmt.Println("Posting answers to adventofcode.com")
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("https://adventofcode.com/%d/day/%d/answer", year, day), strings.NewReader(fmt.Sprintf("level=%d&answer=%s", level, answer)))
+	if err != nil {
+		panic(err)
+	}
+
+	cookie := new(http.Cookie)
+	cookie.Name = "session"
+	cookie.Value = token
+	req.AddCookie(cookie)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	if res.StatusCode != 200 {
+		fmt.Println(string(body))
+		panic("Failed to post answers")
+	}
+
+	return strings.Contains(string(body), "the right answer")
+}
+
+func year2021(day, level int) {
 	input := readInput(2021, day)
 	fmt.Println()
 
-	days := []func(){
-		func() {
-			lastNum, increases := -1, -1
+	days := []func() string{
+		func() string {
+			if level == 1 {
+				lastNum, increases := -1, -1
 
-			for _, line := range input {
-				num, err := strconv.Atoi(line)
+				for _, line := range input {
+					num, err := strconv.Atoi(line)
 
-				if err != nil {
-					continue
+					if err != nil {
+						continue
+					}
+
+					if num > lastNum {
+						increases++
+					}
+
+					lastNum = num
+
 				}
 
-				if num > lastNum {
-					increases++
-				}
-
-				lastNum = num
-
+				fmt.Printf("Output: %d\n", increases)
+				return fmt.Sprint(increases)
 			}
-
-			fmt.Printf("Part 1 Output: %d\n", increases)
 
 			length := len(input)
 			prevSum, increases := -1, -1
@@ -166,60 +283,68 @@ func year2021(day int) {
 				prevSum = sum
 			}
 
-			fmt.Printf("Part 2 Output: %d\n", increases)
+			fmt.Printf("Output: %d\n", increases)
+			return fmt.Sprint(increases)
 		},
-		func() {
-			depth, distance := 0, 0
-			for _, line := range input {
-				if strings.HasPrefix(line, "up ") {
-					depthChange, err := strconv.Atoi(strings.TrimPrefix(line, "up "))
+		func() string {
+			if level == 1 {
+				depth, distance := 0, 0
+				for _, line := range input {
+					split := strings.Split(line, " ")
+					if len(split) != 2 {
+						continue
+					}
+					amount, err := strconv.Atoi(split[1])
 					if err != nil {
 						continue
 					}
-					depth -= depthChange
-				} else if strings.HasPrefix(line, "down ") {
-					depthChange, err := strconv.Atoi(strings.TrimPrefix(line, "down "))
-					if err != nil {
-						continue
-					}
-					depth += depthChange
-				} else if strings.HasPrefix(line, "forward ") {
-					distanceChange, err := strconv.Atoi(strings.TrimPrefix(line, "forward "))
-					if err != nil {
-						continue
-					}
-					distance += distanceChange
-				}
 
+					switch split[0] {
+					case "up":
+						depth -= amount
+					case "down":
+						depth += amount
+					case "forward":
+						distance += amount
+					}
+
+				}
+				fmt.Printf("Output: %d\n", depth*distance)
+				return fmt.Sprint(depth * distance)
 			}
-			fmt.Printf("Part 1 Output: %d\n", depth*distance)
 
 			depth, distance, aim := 0, 0, 0
 			for _, line := range input {
-				if strings.HasPrefix(line, "up ") {
-					aimChange, err := strconv.Atoi(strings.TrimPrefix(line, "up "))
-					if err != nil {
-						continue
-					}
-					aim -= aimChange
-				} else if strings.HasPrefix(line, "down ") {
-					aimChange, err := strconv.Atoi(strings.TrimPrefix(line, "down "))
-					if err != nil {
-						continue
-					}
-					aim += aimChange
-				} else if strings.HasPrefix(line, "forward ") {
-					distanceChange, err := strconv.Atoi(strings.TrimPrefix(line, "forward "))
-					if err != nil {
-						continue
-					}
-					distance += distanceChange
-					depth += aim * distanceChange
+				split := strings.Split(line, " ")
+				if len(split) != 2 {
+					continue
+				}
+				amount, err := strconv.Atoi(split[1])
+				if err != nil {
+					continue
+				}
+
+				switch split[0] {
+				case "up":
+					aim -= amount
+				case "down":
+					aim += amount
+				case "forward":
+					distance += amount
+					depth += aim * amount
 				}
 			}
-			fmt.Printf("Part 2 Output: %d\n", depth*distance)
+			fmt.Printf("Output: %d\n", depth*distance)
+			return fmt.Sprint(depth * distance)
 		},
 	}
 
-	days[day-1]()
+	answer := days[day-1]()
+	if answer != "" {
+		if postAnswers(2021, day, level, answer) {
+			fmt.Println("Solved day", day, "level", level)
+		} else {
+			fmt.Println("Failed to solve day", day, "level", level)
+		}
+	}
 }
